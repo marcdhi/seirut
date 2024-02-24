3aebca183fe8db86db6e4b483083f2734c77f5db
marcdhi <gandhi.mardav@gmail.com>
2024-02-25 01:19:07 +0530

    seirut: about to implement a Lexer

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-use std::{fs::File, process::exit};
+use std::{collections::HashMap, fs::{self, read, File}, hash::Hash, io, path::Path, process::exit};
 use xml::{EventReader, reader::XmlEvent};
 
-fn main() {
-    let file_path = "docs.gl/gl4/glClear.xhtml";
+fn index_document(_doc_content: &str) -> HashMap<String, usize> {
+    todo!("not done yet!")
+}
 
-    let file = File::open(file_path).unwrap_or_else(|err| {
-        eprintln!("Error: could not read file {file_path}: {err}");
-        exit(1)
-    });
-
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
+    let file = File::open(file_path)?;
     let er = EventReader::new(file);
     let mut content = String::new();
-
+    
     for event in er.into_iter(){
-
-        let event = event.unwrap_or_else(|err| {
-            eprintln!("ERROR: Could not read the next event : {err}");
-            exit(1);
-        });
-
-        if let XmlEvent::Characters(text) = event {
+        if let XmlEvent::Characters(text) = event.unwrap() {
             content.push_str(&text);
-            // println!("{text:?}");
         };
+    }
+    Ok(content) 
+}
 
+fn main() -> io::Result<()>{
+    let dir_path = "docs.gl/gl4";
 
+    let dir = fs::read_dir(dir_path);
+
+    for file in dir.unwrap() {
+        let file_path = file.unwrap().path();
+        let content = read_entire_xml_file(&file_path).unwrap();
+        println!("{file_path:?} => {size}", size = content.len());
     }
 
-    println!("{content:?}");
-
-
+    Ok(())
+    // println!("{content}", content = read_entire_xml_file(file_path).unwrap());
 }
  
